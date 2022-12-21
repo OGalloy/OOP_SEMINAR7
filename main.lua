857f4b2c7ab077496ec3b227087ea99029105da9
OGalloy <56289594+OGalloy@users.noreply.github.com>
2022-12-21 11:45:52 +0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,17 +1,38 @@
---путь во папки data
+--путь до папки data
 package.path = "./data/?.lua"..package.path
 require("complex_number")
 
-print(Complex:new(123, 23):getValue())
+print("Программа - калькулятор комплексных чисел")
 
-
-
---[[comp1 = Complex:new(10, 11)
-comp2 = Complex:new(1, 2)
-
-print(comp1:getValue())
-print(comp2:getValue())
-comp3 = comp1:add(comp2)
-comp4 = comp1:sub(comp2)
-print(comp3:getValue())
-print(comp4:getValue())]]--
+while true do
+  print("Введите вещественную часть первого числа")
+  num1Real = io.read()
+  print("Введите мнимую часть первого числа")
+  num1Imaginary = io.read()
+  complexValue1 = Complex:new(num1Real, num1Imaginary)
+  
+  print("Введите вещественную часть второго числа")
+  num2Real = io.read()
+  print("Введите мнимую часть второго числа")
+  num2Imaginary = io.read()
+  complexValue2 = Complex:new(num2Real, num2Imaginary)
+  
+  print("Введите Операцию")
+  print("add - сложение")
+  print("sub - разность")
+  print("mul - умножение")
+  operator = io.read()
+  result = nil
+  if operator == "add" then
+    result = complexValue1:add(complexValue2)
+  elseif operator == "sub" then
+    result = complexValue1:sub(complexValue2)
+  elseif operator == "mul" then
+    --result = complexValue1.mul(complexValue2)
+  else
+    print("Неизвестный оператор")
+  end
+  print(string.format("(%d+%di) + (%d+%di) = %d+%di", complexValue1:getRealPart(), complexValue1:getImaginaryPart(), 
+                                                  complexValue2:getRealPart(), complexValue2:getImaginaryPart(),
+                                                  result:getRealPart(), result:getImaginaryPart()))
+end
